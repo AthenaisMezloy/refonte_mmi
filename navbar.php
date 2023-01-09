c6d0dd5207b942540713866439596b060ded1948
Lilian <lilianpeuron@gmail.com>
2023-01-09 15:37:47 +0100

    09/01/2023
    ajouts :
    index
    model
    navbar
    style
    img

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav>
+    <div>
+        <img src="./img/assets/logo_img.svg" alt="">
+    </div>
+    <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">La Formation</a></li>
+        <li><a href="">Projets</a></li>
+        <li><a href="">Après MMI</a></li>
+        <li><a href="">International</a></li>
+        <li><a href="">À propos</a></li>
+    </ul>
+</nav>
