172cb02d4272b4ddbec736b7b276b7ed7d71bf2a
Lilian <lilianpeuron@gmail.com>
2023-01-09 17:46:31 +0100

    17:46 09/01/2023

--- index.php
+++ index.php
@@ -7,15 +7,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins&display=swap" rel="stylesheet">    
     <link rel="stylesheet" href="style.css">
     <title>BUT MMI Champs-sur-Marne</title>
 </head>
 
 <body>
-    <?php 
+
+    <div class="btn-section">
+        <ul>
+            <a href="#home"><li></li></a>
+            <a href="#intro"><li></li></a>
+            <a href=""><li></li></a>
+            <a href=""><li></li></a>
+            <a href=""><li></li></a>
+            <a href=""><li></li></a>
+        </ul>
+    </div>
+
+    <section id="home">
+        <?php 
         include("./navbar.php")
-    ?>
+        ?>
+        <div class="block">
+        </div>
+        <div class="title">
+            <h1>BUT MMI</h1>
+            <p>Métiers du Multimédia et de l'Internet</p>
+        </div>
+    </section>
+
+    <section id="intro">
+        <div class="title">
+            <h2>Qu'est-ce que <br> MMI?</h2>
+        </div>
+        <div class="content1">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </section>
+
 </body>
 
 </html>
