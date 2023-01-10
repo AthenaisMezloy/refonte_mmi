f88f73285e195b547bd99e521d8951f3edac0b5e
Lilian <lilianpeuron@gmail.com>
2023-01-10 17:46:22 +0100

    10/01/2023 p.1

---
+++ international.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="international-style.css">
+    <title>BUT MMI Champs - International</title>
+</head>
+<body>
+    <?php 
+        include("./navbar.php")
+    ?>
+
+    <section id="home">
+        <h1>
+            International
+        </h1>
+    </section>
+
+    <section>
+        <h2>What’s MMI?</h2>
+        <div class="content">
+            <div class="img-container">
+                <img src="#" alt="">
+            </div>
+            <div class="text-container">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>One Title?</h2>
+        <div class="content">
+            <div class="text-container">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div class="img-container">
+                <img src="#" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <h2>Québec’s travel</h2> 
+        <div class="content">
+            <div class="img-container">
+                <img src="#" alt="">
+            </div>
+            <div class="text-container">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+    </section>
+
+    
+    <section id="partnerships">
+            <h3>Partnerships</h3>
+            <div>
+                <div class="UQAC">
+                    <p>UQAC</p>
+                    <div>
+
+                    </div>
+                </div>
+                <div class="CEGEP">
+                    <p>CÉGEP</p>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+        <h3>How to join MMI ?</h3>
+
+        <h4>Étape 1</h4>
+        <h4>Étape 2</h4>
+        <h4>Étape 3</h4>
+
+    <?php 
+        include("./footer.php")
+    ?>
+</body>
+</html>
--- navbar.php
+++ navbar.php
@@ -3,11 +3,11 @@
         <img src="./img/assets/logo_img.svg" alt="">
     </div>
     <ul>
-        <li><a href="">Accueil</a></li>
+        <li><a href="./index.php">Accueil</a></li>
         <li><a href="">La Formation</a></li>
         <li><a href="./project.php">Projets</a></li>
         <li><a href="">Après MMI</a></li>
-        <li><a href="">International</a></li>
+        <li><a href="./international.php">International</a></li>
         <li><a href="">À propos</a></li>
     </ul>
 </nav>
