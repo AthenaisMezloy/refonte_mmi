23e5ad4e98cfa43cc3153b4827fc50628cd088ac
Lilian <lilianpeuron@gmail.com>
2023-01-10 13:42:09 +0100

    séparation nav footer

--- footer.php
+++ footer.php
@@ -33,3 +33,55 @@
         <a href="#home"><p>HAUT DE PAGE</p></a>
     </div>
 </footer>
+<style>
+    footer {
+    background-color: white;
+    width: 100%;
+    height: 40vh;
+    padding: 0.5em 1em 0.5em 1em;
+}
+
+.info-container {
+    display: flex;
+    width: 100%;
+    height: 80%;
+}
+
+.info-container div {
+    width: 50%;
+}
+
+.info-container div h2 {
+    text-align: center;
+}
+
+.info-container ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 70%;
+}
+
+.map-content {
+    padding: 0 0 0 1em;
+}
+
+.law-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 20%;
+    font-family: 'Poppins', sans-serif;
+    font-size: 0.8em;
+}
+
+.law-content h2 {
+    font-size: 1.5em;
+}
+
+.law-content a {
+    color: black;
+}
+</style>
