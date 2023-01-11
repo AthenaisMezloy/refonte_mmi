17d75d5afcdcd8ff689864268dbbc2e6a167bfa2
Lilian <lilianpeuron@gmail.com>
2023-01-11 09:53:02 +0100

    11/01/2023
    page international
    no animation lorem ipsum

--- international.php
+++ international.php
@@ -21,7 +21,7 @@
         </h1>
     </section>
 
-    <section>
+    <section id="intro">
         <h2>What’s MMI?</h2>
         <div class="content">
             <div class="img-container">
@@ -35,7 +35,7 @@
         </div>
     </section>
 
-    <section>
+    <section id="one-title">
         <h2>One Title?</h2>
         <div class="content">
             <div class="text-container">
@@ -49,7 +49,7 @@
         </div>
     </section>
 
-    <section>
+    <section id="quebec">
         <h2>Québec’s travel</h2> 
         <div class="content">
             <div class="img-container">
@@ -68,26 +68,65 @@
             <h3>Partnerships</h3>
             <div>
                 <div class="UQAC">
-                    <p>UQAC</p>
-                    <div>
-
-                    </div>
+                    <h4>UQAC</h4>
+                    <p>
+                    Texte de présentation
+                    </p>
                 </div>
                 <div class="CEGEP">
-                    <p>CÉGEP</p>
-                    <div>
-
-                    </div>
+                    <h4>CÉGEP</h4>
+                    <p>
+                    Texte de présentation
+                    </p>
                 </div>
             </div>
 
     </section>
 
+    <section id="join-mmi">
         <h3>How to join MMI ?</h3>
+        <ul>
+            <li>
+                <p>Étape 1</p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L
+                </p>
+            </li>
+            <li>
+                <p>Étape 2</p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L
+                </p>
+            </li>
+            <li>
+                <p>Étape 3</p>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L
+                </p>
+            </li>
+        </ul>
 
-        <h4>Étape 1</h4>
-        <h4>Étape 2</h4>
-        <h4>Étape 3</h4>
+    </section>
+
+    <section id="faq">
+        <h2>Questions & Answers</h2>
+
+        <ul>
+            <li>
+                <p>Can you aks a question ?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L</p>
+            </li>
+            <li>
+                <p>To be or not to be ?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L</p>
+            </li>
+            <li>
+                <p>MMI, is it a wonderful bachelor ?</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.L</p>
+            </li>
+        </ul>
+    </section>
+
 
     <?php 
         include("./footer.php")
