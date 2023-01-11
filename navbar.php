ec0f9b686ffb3d3fdec355d76f6479bbda480392
Lilian <lilianpeuron@gmail.com>
2023-01-11 10:14:55 +0100

    nav-footer changes

--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="footer-nav-style.css">
 <nav>
     <div>
         <img src="./img/assets/logo_img.svg" alt="">
@@ -11,39 +12,3 @@
         <li><a href="">À propos</a></li>
     </ul>
 </nav>
-<style>
-    nav {
-        color: white;
-        position: absolute;
-        font-family: 'Poppins', sans-serif;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        height: 8vh;
-        padding: 0.2em 2em 0 1em;
-        font-size: 0.8em;
-        z-index: 2;
-        mix-blend-mode: difference;
-    }
-
-    nav a {
-        color: white;
-    }
-
-    nav div {
-        width: 30%;
-    }
-
-    nav div img {
-        width: 25%;
-        height: auto;
-        padding: 0.2em;
-    }
-
-    nav ul {
-        display: flex;
-        justify-content: space-between;
-        width: 60%;
-    }
-</style>
