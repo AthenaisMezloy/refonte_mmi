faa460d4ceb883239934433aef88da8d5dfdd917
Lilian <lilianpeuron@gmail.com>
2023-01-13 11:56:50 +0100

    mentions légales

--- legal-notice.php
+++ legal-notice.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,6 +11,7 @@
     <link rel="stylesheet" href="legal-notice-style.css">
     <title>MMI Champs - Mentions Légales</title>
 </head>
+
 <body>
     <?php 
         include("./navbar.php")
@@ -20,13 +22,103 @@
     <section id="notice-list">
         <ul>
             <li>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <h2>Éditeur du site - TABL</h2>
+                <p>Département MMI, IUT de Marne-la-Vallée - Université Gustave Eiffel
+                    Contributeurs : Timothé BUREAU, Athénaïs MEZLOY, Bruno MARTINS DA COSTA, Lilian PEURON</p>
+
             </li>
             <li>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <h2>Siège social</h2>
+                <p>
+                    2 Rue Albert Einstein, 77420 Champs-sur-Marne (FR)
+                </p>
             </li>
             <li>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <h2>Hébergeur</h2>
+                <p>
+                    <a href="https://www.ionos.fr/">ionos.fr</a>  <br>
+                    7 Pl. de la Gare, 57200 Sarreguemines
+                </p>
+            </li>
+            <li>
+            <h2>Habillage du site</h2>
+            <p>
+                L’habillage du site a été réalisé par Athénaïs MEZLOY, Lilian PEURON, Bruno MARTINS DA COSTA et Timothé
+                BUREAU.
+            </p>
+            </li>
+            <li>
+            <h2>DPO-Collecte de données</h2>
+            <p>
+                Athénaïs MEZLOY <br>
+                Adresse courriel : athenais.mezloy@gmail.com <br>
+                Adresse postale : 21 chemin des Chênes, 93160 Noisy-le-Grand (FR) <br><br>
+
+                Le DPO peut exercer sur les données recueillies le droit de modification, le droit d’accès, le droit de
+                suppression, le droit d’oubli ainsi que le droit à la portabilité. <br><br>
+
+                Lorsque les visiteurs laissent des commentaires sur le site, nous recueillons les données indiquées dans
+                le formulaire de commentaires, ainsi que l’adresse IP du visiteur et la chaîne de l’agent utilisateur du
+                navigateur pour aider à détecter les pourriels. <br>
+                Une chaîne anonyme créée à partir de votre adresse e-mail (également appelée hash) peut être fournie au
+                service Gravatar pour voir si vous l’utilisez. La politique de confidentialité du service Gravatar est
+                disponible ici : <a href="https://automattic.com/privacy/">https://automattic.com/privacy/</a>. Après l’approbation de votre commentaire, votre photo
+                de profil est visible pour le public dans le contexte de votre commentaire. <br><br>
+
+                Les données confiées sont utilisées dans le strict cadre du fonctionnement de notre site. <br>
+
+                Ce site n’est pas concerné par le RGPD.
+
+            </p>
+            </li>
+            <li>
+            <h2>Contenu intégré d'autres sites web</h2>
+            <p>
+                Les pages de ce site peuvent inclure du contenu intégré (par exemple des vidéos, images, articles,
+                etc.). Le contenu intégré d’autres sites Web se comporte exactement de la même façon que si le visiteur
+                a visité l’autre site Web.
+                Ces sites Web peuvent recueillir des données vous concernant, utiliser des cookies, intégrer un suivi
+                tiers supplémentaire et surveiller votre interaction avec ce contenu intégré, y compris le suivi de
+                votre interaction avec le contenu intégré si vous avez un compte et que vous êtes connecté à ce site
+                Web.
+            </p>
+            </li>
+            <li>
+            <h2>Autorisation du droit à l’image pour les vidéos et images</h2>
+            <p>Chaque personne dont l'image apparaît sur le site certifie l’autorisation du droit à l’image.</p>
+            </li>
+            <li>
+            <h2>Médias</h2>
+            <p>Les images téléchargées sur notre site ne pourront faire l’objet d’aucune autre utilisation. La
+                responsabilité de notre site n’est en aucun cas engagée.</p>
+            </li>
+            <li>
+            <h2>Source des images</h2>
+            <p>
+                audiovisuel.webp : <a href="https://www.flaticon.com/free-icons/video" title="video icons">Video icons
+                    created by Freepik - Flaticon</a>
+                <br>
+                communication.webp : <a href="https://www.flaticon.com/free-icons/online-chat"
+                    title="online chat icons">Online chat icons created by Stockes Design - Flaticon</a>
+                <br>
+
+                design.webp : <a href="https://www.flaticon.com/free-icons/graphic-design"
+                    title="graphic design icons">Graphic design icons created by Freepik - Flaticon</a>
+                <br>
+
+                developpement.webp : <a href="https://www.flaticon.com/free-icons/graphic-design"
+                    title="graphic design icons">Graphic design icons created by Freepik - Flaticon</a>
+                <br>
+
+                sun.webp : <a href="https://www.flaticon.com/free-icons/weather" title="weather icons">Weather icons
+                    created by Freepik - Flaticon</a>
+                <br>
+
+                moon.webp : <a href="https://www.flaticon.com/free-icons/moon" title="moon icons">Moon icons created by
+                    Good Ware - Flaticon</a>
+                <br>
+
+                Pas fini !!!</p>
             </li>
         </ul>
     </section>
@@ -34,4 +126,5 @@
         include("./footer.php")
     ?>
 </body>
+
 </html>
