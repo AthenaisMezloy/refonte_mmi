d53e88ed1f34eacedf87403fd2cd82cdb62a7a65
AthenaisMezloy <115143120+AthenaisMezloy@users.noreply.github.com>
2023-01-17 17:32:54 +0100

    Update about.php

--- about.php
+++ about.php
@@ -1,3 +1,6 @@
+<?php
+require("model.php");
+$team = getTeam()?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="about-style.css">
     <title>MMI Champs - À propos</title>
 </head>
@@ -16,28 +21,38 @@
     ?>
     <section id="home">
         <h1>La Team TABL</h1>
-        <p>Citation ? - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <div class="slider">
-            <button class="btn-left" onclick="slide(-1)"><img src="./img/about/arrow_img.svg" alt=""></button>
-            <div class="slider-container">
-                <ul>
-                    <img src="" alt="">
-                    <p></p>
-                </ul>
-                <ul>
-                    <img src="" alt="">
-                    <p></p>
-                </ul>
-                <ul>
-                    <img src="" alt="">
-                    <p></p>
-                </ul>
-            </div>
-            <button class="btn-right" onclick="slide(1)"><img src="./img/about/arrow_img.svg" alt=""></button>
+        <p class="citation">Ils sont sympas ! - Emy B.</p>
+        <div class="swiper">
+        <div class="swiper-wrapper">
+            <?php foreach ($team as $team) : ?>
+                <div class="swiper-slide">
+                    <?php if (!empty($team->photo)) : ?>
+                        <img class="image" src="data:image/webp;base64,<?= base64_encode($team->photo) ?>" alt="<?= $team->name_partner ?>" />
+                    <?php else : ?>
+                        <div class="img_notfound">
+                            <p>No image found.</p>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="team-content">
+                        <h4><?= $team->name_partner ?></h4> <br>
+                        <h4><?= $team->role ?></h4>
+                    </div>
+
+                </div>
+
+            <?php endforeach; ?>
+
         </div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+    </div>
     </section>
     <?php 
         include("./footer.php")
     ?>
 </body>
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+<script src="app.js"></script>
 </html>
