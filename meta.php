69d2398fb658d19ad6c8f77ec815aeb390aa1d3a
Lilian <lilianpeuron@gmail.com>
2023-01-17 19:42:18 +0100

    insert meta.php

---
+++ meta.php
@@ -0,0 +1,19 @@
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Le BUT MMI à Champs-sur-Marne, une formation qui forme aux métiers du web, accessible à de nombreux profils, technologiques, généraux, etc..." />
+    <meta name='date' content='Jan. 18, 2023'>
+    <meta name = "keywords" content = "Formation, Multimédia, Internet, Digital, Étudiant, MMI, Développement Web, Design, Audiovisuel, Communication" />
+    <meta name='copyright' content='BUT MMI Champs'> 
+<meta property="og:locale" content="fr_FR" />
+    <meta property="og:type" content="siteweb" />
+    <meta property="og:title" content="Accueil - BUT MMI" />
+    <meta property="og:image" content="https://mmichamps.fr/img/logo.png">
+    <meta property="og:url" content="http://butmmichamps.fr/" />
+    <meta property="og:site_name" content="BUT MMI" />
+    <meta property="og:description" content="Le BUT MMI à Champs-sur-Marne, une formation qui forme aux métiers du web, accessible à de nombreux profils, technologiques, généraux, etc...">
+    <link rel="icon" type="image/svg" href="./img/icons/favicon.svg">
+    <link rel="shortcut icon" type="image/x-icon" href="./img/icons/favicon.svg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Poppins&display=swap" rel="stylesheet">
